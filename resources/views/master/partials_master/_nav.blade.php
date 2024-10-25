88e1053985af74f2df069b9c4cf78a4d06b82bb7
joaquimmulaza <joaquimmulazadev@gmail.com>
2024-10-25 06:54:12 +0100

    solved bug in navbar

--- resources/views/master/partials_master/_nav.blade.php
+++ resources/views/master/partials_master/_nav.blade.php
@@ -17,7 +17,7 @@
         <div class="contentRight">
             {{-- CLASSES QUE SOME ITEM DA LISTA EM TAMANHO SM: d-none d-sm-inline-block--}}
             <li class="nav-item">
-                <a class="nav-link {{Route::current()->getName() === 'home' ? 'active' : ''}}"
+                <a  class="nav-link {{Route::current()->getName() === 'home' ? 'active' : ''}}"
                 href="{{route('home')}}"><img style="position: relative; bottom: 10px;" src="logo/img/200x70.svg" alt=""></a>
             </li>
             <li class="nav-item">
@@ -26,45 +26,45 @@
         </div>
         <div class="contentLeft">
             <li class="nav-item">
-                <a class="nav-link {{Route::current()->getName() === 'home' ? 'active' : ''}}"
-                href="{{route('home')}}"><img style="position: relative; bottom: 5px;" src="logo/img/icon/home-button-pressed.svg" alt=""></a>
+                <a style="padding: 0 !important; margin: 0 !important;" class="nav-link {{Route::current()->getName() === 'home' ? 'active' : ''}}"
+                href="{{route('home')}}"><img src="logo/img/icon/home-button-pressed.svg" alt=""></a>
             </li>
             
             
             @if(solicitacoes() > 0)
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('user.pedidos')}}"> {{-- Redirecionar para a página de pedidos de férias --}}
+                    <a style="padding: 0 !important; margin: 0 !important;" class="nav-link" href="{{route('user.pedidos')}}"> {{-- Redirecionar para a página de pedidos de férias --}}
                         <span class="badge badge-warning navbar-badge">
                             {{solicitacoes()}}
                         </span>
-                    <img style="position: relative; bottom: 5px;" src="logo/img/icon/Notification-button.svg" alt="">
+                    <img src="logo/img/icon/Notification-button.svg" alt="">
                     </a>
                 </li>
                 @else
                     <li class="nav-item">
-                    <a class="nav-link" data-toggle="dropdown">
+                    <a style="padding: 0 !important; margin: 0 !important;" class="nav-link" data-toggle="dropdown">
                         <span class="badge badge-success navbar-badge">
                             {{solicitacoes()}}
                         </span>
-                        <img style="position: relative; bottom: 5px;" src="logo/img/icon/Notification-button.svg" alt="">
+                        <img src="logo/img/icon/Notification-button.svg" alt="">
                     </a>
                 </li>
             @endcan
             <li class="nav-item">
-                <a class="nav-link"
-                href="#" class="nav-link"><img style="position: relative; bottom: 5px;" src="logo/img/icon/user-avatar.svg" alt=""></a>
+                <a style="padding: 0 !important; margin: 0 !important;" class="nav-link"
+                href="#" class="nav-link"><img src="logo/img/icon/user-avatar.svg" alt=""></a>
             </li>
-            <span style="padding-right: 18px; position: relative; bottom: -2px;"><img src="logo/img/icon/line-1.svg" alt=""></span>
+            <span><img src="logo/img/icon/line-1.svg" alt=""></span>
             @can('app.dashboard')
             <li class="nav-item btnCadastrar">
-                <a class="nav-link {{Route::current()->getName() === 'admin' ? 'active' : ''}}"
+                <a style="padding: 0 !important; margin: 0 !important;" class="nav-link {{Route::current()->getName() === 'admin' ? 'active' : ''}}"
                 href="#" class="nav-link colorLink">Cadastrar</a>
             </li>
         
             <li class="nav-item">
-                <a style="padding-right: 0 !important" class="nav-link {{Route::current()->getName() === 'admin.logout' ? 'active' : ''}}"
+                <a style="padding: 0 !important; margin: 0 !important;" class="nav-link {{Route::current()->getName() === 'admin.logout' ? 'active' : ''}}"
                 data-toggle="tooltip" title="Sair do sistema" href="{{route('admin.logout')}}" class="nav-link">
-                <img style="position: relative; bottom: 4px;" src="logo/img/icon/Menu.svg" alt=""></a>
+                <img src="logo/img/icon/Menu.svg" alt=""></a>
             </li>
 
             {{-- HELPER PARA SOLICITAÇÕES DE PEDIDOS --}}
