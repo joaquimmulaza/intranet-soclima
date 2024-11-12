ba5b1f23248990de48a871d2d5fe62efd03c05cb
joaquimmulaza <joaquimmulazadev@gmail.com>
2024-11-12 16:14:51 +0100

    dropdown menu created

--- resources/views/master/partials_master/_nav.blade.php
+++ resources/views/master/partials_master/_nav.blade.php
@@ -1,3 +1,7 @@
+<style>
+    
+</style>
+
 <nav class="main-header navbar navbar-expand navbar-white heightNav justify-content-start" style="margin: 0 !important;">
 
     <!-- MENU ESQUERDO ADMINISTRATIVO -->
@@ -46,17 +50,41 @@
                 </li>
                 @else
                     <li class="nav-item">
-                    <a style="padding: 0 !important; margin: 0 !important;" class="nav-link" data-toggle="dropdown">
+                    <a style="padding: 0 !important; margin: 0 !important;" class="nav-link notificacao" data-toggle="dropdown" id="notificationDropdownToggle">
                         <!-- <span class="  navbar-badge">
                             {{solicitacoes()}}
                         </span> -->
                         <img src="{{asset('logo/img/icon/Notification-button.svg')}}" alt="">
                     </a>
+                    <div class="dropdown-content" id="notificationDropdown">
+                    <h5>Notificações</h5>
+                    <hr>
+                    <div class="menuOpt">
+                        @if(solicitacoes() > 0)
+                            <a href="{{route('user.pedidos')}}"><img src="logo/img/icon/notification-icon.svg" alt="">Pedidos de Férias</a>
+                        @else
+                            <span>Sem novas notificações</span>
+                        @endif
+                    </div>
+                </div>
                 </li>
             @endcan
             <li class="nav-item">
-                <a style="padding: 0 !important; margin: 0 !important;" class="nav-link"
-                href="#" class="nav-link"><img src="{{asset('logo/img/icon/user-avatar.svg')}}" alt=""></a>
+                <a style="padding: 0 !important; margin: 0 !important;" class="nav-link myProfile"
+                href="#" class="nav-link"><img src="{{asset('logo/img/icon/user-avatar.svg')}}" alt="" id="profileDropdownToggle">
+                
+            </a>
+            <div class="dropdown-content" id="profileDropdown">
+                <h5>Meu Perfil</h5>
+                <hr>
+                <div class="menuOpt">
+                    <a href="">Ver Perfil</a>
+                    <a href="">Configurações</a>
+                    <a href="">Sair</a>
+                </div>
+            </div>
+
+                
             </li>
             <span><img src="{{asset('logo/img/icon/line-1.svg')}}" alt=""></span>
             @can('app.dashboard')
@@ -65,10 +93,32 @@
                 href="{{route('user.create')}}" class="nav-link colorLink">Cadastrar</a>
             </li>
         
-            <li class="nav-item">
-                <a style="padding: 0 !important; margin: 0 !important;" class="nav-link {{Route::current()->getName() === 'admin.logout' ? 'active' : ''}}"
-                data-toggle="tooltip" title="Sair do sistema" href="{{route('admin.logout')}}" class="nav-link">
-                <img src="{{asset('logo/img/icon/Menu.svg')}}" alt=""></a>
+            <li class="nav-item menu">
+                <a id="menuDropdownToggle" style="padding: 0 !important; margin: 0 !important;" class="nav-link {{Route::current()->getName() === 'admin.logout' ? 'active' : ''}}"
+                data-toggle="tooltip" title="Sair do sistema" href="#">
+                    <img src="{{asset('logo/img/icon/Menu.svg')}}" alt="">
+                </a>
+                    <!-- Dropdown Content -->
+                    <div class="dropdown-content" id="menuDropdown">
+                        <h5>Menu</h5>
+                        <hr>
+                        <div class="menuOpt">
+                            <a href="#"><img src="logo/img/icon/calendar-star.svg" alt="">Eventos</a>
+                            <span>Gerenciar</span>
+                            <a class="{{Route::current()->getName() === 'user.index' ? 'menu-open' : ''}}" href="{{route('user.index')}}"><img src="logo/img/icon/managerUser.svg" alt="">Gerir usuários</a>
+                            <a href="#"><img src="logo/img/icon/holiday-icon.svg" alt="">Pedidos férias</a>
+                            <a href="#"><img src="logo/img/icon/feeds.svg" alt="">Publicações e atividades</a>
+                        </div>
+
+                        <div class="menuOpt">
+                            <span>Geral</span>
+                            <a href="#"><img src="logo/img/icon/recibos-icon.svg" alt="">Recibos</a>
+                            <a href="#"><img src="logo/img/icon/justificativo-icon.svg" alt="">Justificativos</a>
+                            <a class="{{Route::current()->getName() === 'telefones.index' ? 'menu-open' : ''}}" href="{{route('telefones.index')}}"><img src="logo/img/icon/list-phone.svg" alt="">Lista telefônica</a>
+                            <a href="#"><img src="logo/img/icon/ferias-icon.svg" alt="">Férias</a>
+                        </div>
+                    </div>
+               
             </li>
 
             {{-- HELPER PARA SOLICITAÇÕES DE PEDIDOS --}}
@@ -80,4 +130,34 @@
 
     </ul>
 
+    
+
 </nav>
+
+
+<script>
+   
+
+    // Função para controlar a exibição de um dropdown específico
+    function toggleDropdown(dropdownId, toggleButtonId) {
+        const dropdown = document.getElementById(dropdownId);
+        const toggleButton = document.getElementById(toggleButtonId);
+
+        toggleButton.addEventListener('click', function(event) {
+            event.preventDefault(); // Previne comportamento padrão
+            dropdown.classList.toggle('show'); // Alterna a exibição do dropdown
+        });
+
+        // Fecha o dropdown ao clicar fora dele
+        window.addEventListener('click', function(event) {
+            if (!toggleButton.contains(event.target) && !dropdown.contains(event.target)) {
+                dropdown.classList.remove('show');
+            }
+        });
+    }
+
+    // Inicializa os dropdowns
+    toggleDropdown('notificationDropdown', 'notificationDropdownToggle');
+    toggleDropdown('profileDropdown', 'profileDropdownToggle');
+    toggleDropdown('menuDropdown', 'menuDropdownToggle');
+</script>
